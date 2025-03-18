04bdf1cadbfe890de3b58183012bd76777dd2ccd
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-18 16:07:10 -0300

    refactor: se mejoro el dashboard del administrador

--- frontend/src/types/backendTypes.ts
+++ frontend/src/types/backendTypes.ts
@@ -6,28 +6,60 @@ export interface DoctorData {
     profile_img: string
 }
 
+export enum Gender {
+    MALE = "MALE",
+    FEMALE = "FEMALE"
+}
+
 export interface PacienteResponse {
     patients: PacienteData[]
 }
 
 export interface PacienteData {
-    id:                   number;
-    firstName:            string;
-    lastName:             string;
-    documentId:           string;
-    birthDate:            string;
-    gender:               string;
-    phone:                string;
-    address:              string;
+    id: number;
+    firstName: string;
+    lastName: string;
+    documentId: string;
+    birthDate: string;
+    gender: string;
+    phone: string;
+    address: string;
     emergencyContactInfo: string;
 }
 
 export interface MedicacionData {
-    id:             number;
+    id: number;
     medicationName: string;
-    dosage:         string;
-    frequency:      string;
-    startDate:      string;
-    endDate:        string;
-    notes:          string;
+    dosage: string;
+    frequency: string;
+    startDate: string;
+    endDate: string;
+    notes: string;
+}
+
+export enum Speciality {
+    CLINICA = "CLINICA",
+    CARDIOLOGIA = "CARDIOLOGIA",
+    NEUROLOGIA = "NEUROLOGIA",
+    PSIQUIATRIA = "PSIQUIATRIA",
+    PSICOLOGIA = "PSICOLOGIA",
+    NUTRICION = "NUTRICION",
+    DERMATOLOGIA = "DERMATOLOGIA",
+    GINECOLOGIA = "GINECOLOGIA"
+}
+
+export interface MedicoResponse {
+    medics: MedicoData[]
+}
+
+export interface MedicoData {
+    id: number;
+    name: string;
+    lastname: string;
+    description: string;
+    state: string;
+    documentId: string;
+    gender: Gender;
+    speciality: Speciality;
+    phone: string;
 }
